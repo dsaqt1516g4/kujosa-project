7e28c40163e812adf84433c1d6d72573a2225c0c
Joan <joanjerezt@gmail.com>
2015-12-02 15:04:11 +0100

    comment class

--- kujosa-service/src/main/java/edu/upc/eetac/dsa/kujosa/KujosaMediaType.java
+++ kujosa-service/src/main/java/edu/upc/eetac/dsa/kujosa/KujosaMediaType.java
@@ -10,4 +10,8 @@ public interface KujosaMediaType {
     public final static String KUJOSA_STING_COLLECTION = "application/vnd.dsa.kujosa.sting.collection+json";
     public final static String KUJOSA_ROOT = "application/vnd.dsa.kujosa.root+json";
     public final static String KUJOSA_ERROR = "application/vnd.dsa.kujosa.error+json";
+    public final static String KUJOSA_API_COMMENT = "application/vnd.dsa.kujosa.comment+json";
+    public final static String KUJOSA_API_COMMENT_COLLECTION = "application/vnd.dsa.kujosa.comment.collection+json";
+    public final static String KUJOSA_API_RATIO_COLLECTION = "application/vnd.dsa.kujosa.ratio.collection+json";
+    public final static String KUJOSA_API_RATIO = "application/vnd.dsa.kujosa.ratio+json";
 }
