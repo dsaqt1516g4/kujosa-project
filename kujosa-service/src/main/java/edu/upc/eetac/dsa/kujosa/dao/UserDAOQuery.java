02aad6c2358de3d81f56107cebc03d4ff1357e12
Vador05 <sal.beltran.05@gmail.com>
2016-01-17 06:50:41 +0100

    login & Register Repaired

--- kujosa-service/src/main/java/edu/upc/eetac/dsa/kujosa/dao/UserDAOQuery.java
+++ kujosa-service/src/main/java/edu/upc/eetac/dsa/kujosa/dao/UserDAOQuery.java
@@ -14,7 +14,7 @@ public interface UserDAOQuery {
     String CREATE_USER = "insert into users (id, loginid, password, email,fullname, image) values (UNHEX(?), ?, UNHEX(MD5(?)), ?, ?, ?)";
     String UPDATE_USER = "update users set email=?, fullname=? , image=? where id=UNHEX(?)";
     String ASSIGN_ROLE_REGISTERED = "insert into user_roles (userid, role) values (UNHEX(?), 'registered')";
-    String GET_USER_BY_ID = "select hex(u.userid) as id, u.loginid, u.email, u.fullname from users u where id=unhex(?)";
+    String GET_USER_BY_ID = "select hex(u.id) as id, u.loginid, u.email, u.fullname from users u where id=unhex(?)";
     String GET_USER_BY_USERNAME = "select hex(u.id) as id, u.loginid, u.email, u.fullname, u.image from users u where u.loginid=?";
     String DELETE_USER = "delete from users where id= hex(?)";
     String GET_PASSWORD =  "select hex(password) as password from users where id=unhex(?)";
